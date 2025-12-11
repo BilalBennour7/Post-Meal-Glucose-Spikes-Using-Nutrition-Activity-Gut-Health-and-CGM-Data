7fb412e9dabdda9f951ad0d11a7cc7d79dc7906e
BilalBennour7 <bilalbennour1@gmail.com>
2025-12-11 06:27:47 -0500

    Add files via upload

--- Datascience_project/Website.py
+++ Datascience_project/Website.py
@@ -1,28 +1,11 @@
 import streamlit as st
-import pandas as pd
+import joblib
 import numpy as np
+import pandas as pd
 import altair as alt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
 
-df = pd.read_csv("meals_with_spike_labels.csv")
-df = df.dropna(subset=["Spike"])
-
-features = ["Carbs", "Fat", "Protein", "Calories", "Fiber", "Age", "BMI", "Gender"]
-df = df[features + ["Spike"]]
-
-gender_encoder = LabelEncoder()
-df["Gender"] = gender_encoder.fit_transform(df["Gender"].astype(str))
-
-X = df[features]
-y = df["Spike"]
-
-model = RandomForestClassifier(
-    n_estimators=300,
-    max_depth=12,
-    random_state=42
-)
-model.fit(X, y)
+model = joblib.load("Datascience_project/glucose_model.pkl")
+gender_encoder = joblib.load("Datascience_project/gender_encoder.pkl")
 
 st.title("Glucose Spike Predictor")
 st.write("Please enter your meal and personal information to predict if your meal will cause a glucose spike.")
@@ -34,18 +17,17 @@ calories = st.number_input("Calories", min_value=0.0)
 fiber = st.number_input("Fiber (g)", min_value=0.0)
 age = st.number_input("Age", min_value=1.0)
 bmi = st.number_input("BMI", min_value=10.0)
-gender = st.selectbox("Gender", ["Male", "Female"])
+gender = st.selectbox("Gender", options=["Male", "Female"])
 
-gender_map = {"Male": "M", "Female": "F"}
-gender_letter = gender_map[gender]
-gender_encoded = gender_encoder.transform([gender_letter])[0]
+# the model is trained on "M" and "F" instead of male and female this is so that streamlit knows what Male and Female is
+gender_mapping = {"Male": "M", "Female": "F"}
+gender_on_website = gender_mapping[gender]
 
-# Build input array
+gender_encoded = gender_encoder.transform([gender_on_website])[0]
 input_data = np.array([[carbs, fat, protein, calories, fiber, age, bmi, gender_encoded]])
 
-
 if st.button("Predict Spike"):
-    prediction = model.predict(input_data)[0]
+    prediction = model.predict(input_data)
     probability = model.predict_proba(input_data)[0]
 
     if prediction == 1:
@@ -62,12 +44,11 @@ if st.button("Predict Spike"):
         alt.Chart(prob_df)
         .mark_bar(size=60)
         .encode(
-            x="Outcome",
+            x=alt.X("Outcome", sort=None),
             y=alt.Y("Probability", scale=alt.Scale(domain=[0, 1])),
-            color="Outcome",
+            color=alt.Color("Outcome", scale=alt.Scale(scheme="blues")),
             tooltip=["Outcome", "Probability"]
         )
     )
 
     st.altair_chart(chart, use_container_width=True)
-
